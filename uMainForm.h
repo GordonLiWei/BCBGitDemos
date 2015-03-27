c58bec3d0b3e6060e72fc18c59a43940f2a29a83
gordonliwei <gordonliwei@hotmail.com>
2015-03-27 17:50:34 +0800

    Gordon加入一些視覺化UI元件

--- uMainForm.h
+++ uMainForm.h
@@ -6,10 +6,22 @@
 #include <System.Classes.hpp>
 #include <FMX.Controls.hpp>
 #include <FMX.Forms.hpp>
+#include <FMX.Controls.Presentation.hpp>
+#include <FMX.Edit.hpp>
+#include <FMX.StdCtrls.hpp>
+#include <FMX.TabControl.hpp>
+#include <FMX.Types.hpp>
 //---------------------------------------------------------------------------
 class TForm2 : public TForm
 {
 __published:	// IDE-managed Components
+	TToolBar *ToolBar1;
+	TButton *Button1;
+	TTabControl *TabControl1;
+	TTabItem *TabItem1;
+	TTabItem *TabItem2;
+	TTabItem *TabItem3;
+	TEdit *Edit1;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm2(TComponent* Owner);
